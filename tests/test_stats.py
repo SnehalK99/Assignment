dc43038c76610992116de0b9e1ad159b542185da
SnehalK99 <113546705+SnehalK99@users.noreply.github.com>
2022-09-19 15:32:24 +0530

    Code for checking disk & memory usage

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -11,14 +11,15 @@ from pytest import mark
 import logging
 import datetime
 
-logging.basicConfig(filename="log.log",
+
+logging.basicConfig(filename="Allure_Reports.Allure_Reports",
                              filemode = "w+",
                              format = "%(asctime)s : %(levelname)s : %(message)s",
                              datefmt = "%Y-%d-%m %H:%M:%S",
                              level=logging.INFO)
 
 mylogger = logging.getLogger()
-fh = logging.FileHandler('log.log')
+fh = logging.FileHandler('Allure_Reports.Allure_Reports')
 fh.setLevel(logging.DEBUG)
 mylogger.addHandler(fh)
 
@@ -53,13 +54,44 @@ def capture_cpu():
         file1.close()
         return line
 
+def capture_memory():
+    ssh_client = ssh_connect()
+    (stdin, stdout, stderr) = ssh_client.exec_command( "cat /proc/meminfo | grep 'MemTotal'")
+    for line in stdout.readlines():
+
+        Mem = int(line.split(":")[1].lstrip(" ").split(" ")[0])
+        file1 = open("MyFile.txt", "a+")
+        fo = str(Mem)
+        file1.write(fo)
+        timestamp = capture_timestamp()
+        file1.write(timestamp)
+        file1.close()
+        return Mem
+
+def capture_disk():
+    ssh_client = ssh_connect()
+    (stdin, stdout, stderr) = ssh_client.exec_command( "df -h /")
+    output = stdout.readlines()
+    for line in output[1:]:
+        result = line.split()
+        used = (result[2].rstrip("G"))
+        free = (result[3].rstrip("G"))
+        totaldisk = float(used) + float(free)
+        file1 = open("MyFile.txt", "a+")
+        fo = str(totaldisk)
+        file1.write(fo)
+        timestamp = capture_timestamp()
+        file1.write(timestamp)
+        file1.close()
+        return totaldisk
+
 def capture_timestamp():
     ssh_client = ssh_connect()
     (stdin, stdout, stderr) = ssh_client.exec_command("date")
     for line in stdout.readlines():
-
-
         return line
+
+### Test Cases :
 
 @pytest.mark.cli
 def test_cpu_utilisation():
@@ -68,6 +100,16 @@ def test_cpu_utilisation():
     assert cpu_value < 80, "Test Pass"
     mylogger.debug('1 Critical Test Passed')
 
-if __name__ == '__main__':
+@pytest.mark.cli
+def test_memory_utilisation():
+    mylogger.info("Memory Utilisation test case started...")
+    Memory = capture_memory()
+    assert Memory < 2000000, "Test Pass"
 
-    pytest.main(args=['-sv', os.path.abspath(__file__)])
+@pytest.mark.cli
+def test_disk_utilisation():
+    mylogger.info("Disk Utilisation test case started...")
+    disk_usage = capture_disk()
+    assert disk_usage < 90, "Test Pass"
+
+
